8f2665ec1793208301c531200ee41aee7a71884d
Amrit Singh <88683562+techmanmotion@users.noreply.github.com>
2022-06-06 15:57:30 +0530

    Update Categories.jsx

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,86 +1,81 @@
 import React from 'react'
+import { categories, categories2 } from '../data';
 import styled from 'styled-components';
 import './Categories.css';
 import { Link } from "react-router-dom";
+
 
 const Container = styled.div`
   display: grid;
     grid-template-columns: 1fr 1fr 1fr;
 `
+
 const Container2 = styled.div`
+
   display: grid;
   grid-template-columns: 1fr 1fr;
 `
-function openProductlist(){
-    // <Link to="/productlist" />.click();
-}
 
-const shopbtn = <button onClick={openProductlist} className="cat_card_btn">
-<Link to="/productlist" >SHOP NOW</Link>
+
+const shopbtn = <button className="cat_card_btn">
+    <Link to="/productlist" >SHOP NOW</Link>
 </button>;
 
 
-const Category_card = () => {
+
+function Category_card(props) {
+    
+    let arr = props.title.split(" ");
     return (
         <>
-            <Container>
-                <div className="imgBox">
-                          <img className="cat_card_img" alt="Shirt style" src="https://images.pexels.com/photos/5886038/pexels-photo-5886038.jpeg" />
-                    <span className="cat_card_title">
-                        <p >SHIRT <br/> STYLE</p>
-                        {shopbtn}
-                    </span>
-                </div>
-
-                <div className="imgBox">
-                    <img className="cat_card_img" alt="LOUNGEWEAR LOVE" src="https://images.pexels.com/photos/6939081/pexels-photo-6939081.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-                    <span className="cat_card_title">
-                        <p >LOUNGEWEAR <br/> LOVE</p>
-                        {shopbtn}                    </span>
-                </div>
-
-                <div className="imgBox">
-                    <img className="cat_card_img" alt="LIGHT JACKETS" src="https://images.pexels.com/photos/1040945/pexels-photo-1040945.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-                    <span className="cat_card_title">
-                        <p >LIGHT <br/> JACKETS</p>
-                        {shopbtn}                    
-                        </span>
-                </div>
-
-            </Container>
-
-            <Container2>
-                <div className="imgBox">
-                    <img className="cat_card_img" alt="PREFECT JEANS" src="https://images.pexels.com/photos/3769702/pexels-photo-3769702.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-                    <span className="cat_card_title">
-                        <p >PERFECT<br/> JEANS</p>
-                        {shopbtn}                    </span>
-                </div>
-
-                <div className="imgBox">
-                    <img className="cat_card_img" alt="NEW DRESSES" src="https://images.pexels.com/photos/1959853/pexels-photo-1959853.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-                    <span className="cat_card_title">
-                        <p >NEW <br/>DRESSES</p>
-                        {shopbtn}                    </span>
-                </div>
-            </Container2>
-
+            <div className="imgBox" key={props.id}>
+                <img className="cat_card_img" src={props.img} />
+                <span className="cat_card_title">
+                    <p > {arr[0]} <br/> {arr[1]}</p>
+                    {shopbtn}
+                </span>
+            </div>
         </>
-    );
+    )
 }
 
 
 
+function Category_card2(props) {
+    let arr = props.title.split(" ");
+    return (
+        <>
 
+            <div className="imgBox" key={props.id}>
+                <img className="cat_card_img" src={props.img} />
+                <span className="cat_card_title">
+                <p > {arr[0]} <br/> {arr[1]}</p>
+                    {shopbtn}
+                </span>
+            </div>
+
+        </>
+    )
+}
 
 
 
 const Categories = () => {
     return (
-        <div>
-            <Category_card />
+        <>
+            <Container>
 
-        </div>
+                {categories.map(item => (
+                    <Category_card img={item.img} key={item.id} title={item.title} />
+                ))}
+            </Container>
+
+            <Container2>
+                {categories2.map(item => (
+                    <Category_card2 img={item.img} key={item.id} title={item.title} />
+                ))}
+            </Container2>
+        </>
     )
 }
 
