9a66b0b82a675e19eaa09eacc425e1c21b57886a
Amrit Singh <88683562+techmanmotion@users.noreply.github.com>
2022-06-12 01:19:15 +0530

    Add files via upload
    Added Context Hook 2 files stored in this folder

---
+++ src/context/Notestate.js
@@ -0,0 +1,62 @@
+import Notecontext from "./Notecontext";
+import {useState} from 'react';
+
+const Notestate = (props)=>{
+    const [cartItems, setCartItems] = useState([]);
+    const [wishlist, setWishlist] = useState([]);
+    const [cartqty, setCartqty] = useState(0);
+    const [wishlistqty, setWishlistqty] = useState(0);
+
+    function doesExist(item) {
+        return cartItems.includes(item);
+    }
+
+    function addtocart(item) {
+        let tempobj = item;
+        tempobj.cartqty = 1;
+        if (!doesExist(item)) {
+            setCartItems([...cartItems, tempobj]);
+            setCartqty(cartItems.length + 1);
+        }
+    }
+
+    function addtowishlist(item) {
+        let tempobj = item;
+        tempobj.wishlist = true;
+
+        setWishlist([...wishlist, tempobj]);
+        setWishlistqty(wishlist.length + 1);
+
+    }
+
+    function deleteitem(item) {
+        item.cartqty = 0;
+        let remainingItems = cartItems.filter((allitem) => {
+            return (item.id !== allitem.id);
+        })
+        setCartItems(remainingItems);
+        setCartqty(cartqty - 1);
+    }
+
+    function removewishlist(item) {
+        item.wishlist = false;
+        let remainingItems = wishlist.filter((curitem) => {
+            return (item.id !== curitem.id);
+        })
+        setWishlist(remainingItems);
+        setWishlistqty(wishlistqty - 1);
+    }
+
+
+
+
+
+  return (  
+        <Notecontext.Provider value={ {wishlistqty , cartqty , addtocart , removewishlist , cartItems ,deleteitem,wishlist,addtowishlist,    }}>
+            {props.children}
+        </Notecontext.Provider>
+    )
+}
+
+export default Notestate;
+
