a9ff9650fd98a6a4ca06f1e1dcb23e0a89f71f51
Amrit Singh <88683562+techmanmotion@users.noreply.github.com>
2022-06-12 01:17:20 +0530

    Add files via upload
    used Context api Hook  for code improvement

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React,{useState , createContext} from 'react';
 
 import './App.css';
 
@@ -7,75 +6,34 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import MyRouter from './MyRouter';
-
+import Notestate from './context/Notestate';
 function App() {
 
-  const productContext = createContext();
-
-  const[cartItems , setCartItems] = useState([]);
-  const[wishlist , setWishlist] = useState([]);
-
-  const[cartqty , setCartqty] = useState(0);
-    const[wishlistqty , setWishlistqty] = useState(0);
-
-
-  function doesExist(item){
-    return cartItems.includes(item);
+  const islogin = ()=>{
+    return Boolean(sessionStorage.login);
   }
-
-
-  function addtocart(item){
-    let tempobj = item;
-    tempobj.cartqty = 1;
-    if(! doesExist(item)){
-      setCartItems([...cartItems, tempobj]);
-      setCartqty(cartItems.length+1);
-    }
-  }
-
-  function addtowishlist(item){
-    let tempobj = item;
-    tempobj.wishlist = true;
-
-    setWishlist([...wishlist, tempobj]);
-    setWishlistqty(wishlist.length+1);
-    
-  }
-
-  function deleteitem(item){
-    item.cartqty = 0;
-    let remainingItems = cartItems.filter((allitem)=>{
-        return (item.id !== allitem.id); 
-    })
-    setCartItems(remainingItems);
-    setCartqty(cartqty-1);
-  }
-
-  function removewishlist(item){
-    item.wishlist = false;
-    let remainingItems = wishlist.filter((curitem)=>{
-        return (item.id !== curitem.id); 
-    })
-    setWishlist(remainingItems);
-    setWishlistqty(wishlistqty-1);
-  }
-
   return (
     <div className="app">
 
-        <Header cartqty={cartqty} wishlistqty={wishlistqty}/>
+    { console.log(islogin()) }
+        {/* <Header cartqty={cartqty} wishlistqty={wishlistqty}/> */}
 
-      {/* <productContext.provider >
-      </productContext.provider> */}
+    
 
-        <MyRouter cartqty={cartqty} cartItems={cartItems} addtocart={addtocart} deleteitem={deleteitem} wishlist={wishlist} addtowishlist={addtowishlist}  removewishlist={removewishlist}/>
       
-      
-      <Footer/>
-
-
+        {/* <MyRouter cartqty={cartqty} cartItems={cartItems} addtocart={addtocart} deleteitem={deleteitem} wishlist={wishlist} addtowishlist={addtowishlist}  removewishlist={removewishlist}/> */}
+      <Notestate>
+         {(islogin()==='true') ? <Header/> : undefined }
+        <MyRouter/>
+        {(islogin() === 'true') ? <Footer/> : undefined }
+        
+      </Notestate>
     </div>
   );
 }
 
 export default App;
+
+
+
+
--- src/MyRouter.js
+++ src/MyRouter.js
@@ -9,19 +9,30 @@ import Wishlist from './pages/Wishlist';
 import Register from './pages/Register';
 import Login from './pages/Login';
 
+
 const MyRouter = ({cartqty ,cartItems,addtocart , deleteitem , wishlist , addtowishlist , removewishlist}) => {
     return (
         <Routes>
 
-            <Route path="/" element={<Home  cartItems={cartItems} addtocart={addtocart} addtowishlist={addtowishlist} />} />
+            <Route path="/" exact element={<Home/>} />
+            <Route path="/product/:id" element={<Product   />} />
+            <Route path="/wishlist" element={<Wishlist  />} />
+            <Route path="/cart" element={<Cart  />} />
+            <Route path="/productlist" element={<ProductList  />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" index element={<Login />} />
+            <Route path="*"  element={<h1>404 Page not found</h1>} />
 
-            <Route path="/product/:id" element={<Product  cartItems={cartItems} addtocart={addtocart} addtowishlist={addtowishlist} />} />
+
+            {/* <Route path="/" element={<Home  cartItems={cartItems} addtocart={addtocart} addtowishlist={addtowishlist} />} /> */}
+
+            {/* <Route path="/product/:id" element={<Product  cartItems={cartItems} addtocart={addtocart} addtowishlist={addtowishlist} />} />
             <Route path="/wishlist" element={<Wishlist  wishlist={wishlist} addtocart={addtocart} removewishlist={removewishlist}/>} />
             <Route path="/cart" element={<Cart  cartItems={cartItems} addtocart={addtocart} deleteitem={deleteitem} addtowishlist={addtowishlist}/>} />
             <Route path="/productlist" element={<ProductList  cartItems={cartItems}  addtocart={addtocart} addtowishlist={addtowishlist}/>} />
 
             <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
+            <Route path="/login" element={<Login />} /> */}
 
         </Routes>
 
