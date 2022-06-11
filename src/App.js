f877a7614555b6570d8e847646f674bd33d8cca0
Amrit Singh <88683562+techmanmotion@users.noreply.github.com>
2022-06-11 16:52:03 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,38 +1,79 @@
-import React from 'react';
+import React,{useState , createContext} from 'react';
 
 import './App.css';
-import Home from './pages/Home';
-import Product from './pages/Product';
-import ProductList from './pages/ProductList';
-import Cart from './pages/Cart';
-import {Route,Routes} from 'react-router-dom';
-import Register from './pages/Register';
-import Login from './pages/Login';
+
+
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import MyRouter from './MyRouter';
 
 function App() {
+
+  const productContext = createContext();
+
+  const[cartItems , setCartItems] = useState([]);
+  const[wishlist , setWishlist] = useState([]);
+
+  const[cartqty , setCartqty] = useState(0);
+    const[wishlistqty , setWishlistqty] = useState(0);
+
+
+  function doesExist(item){
+    return cartItems.includes(item);
+  }
+
+
+  function addtocart(item){
+    let tempobj = item;
+    tempobj.cartqty = 1;
+    if(! doesExist(item)){
+      setCartItems([...cartItems, tempobj]);
+      setCartqty(cartItems.length+1);
+    }
+  }
+
+  function addtowishlist(item){
+    let tempobj = item;
+    tempobj.wishlist = true;
+
+    setWishlist([...wishlist, tempobj]);
+    setWishlistqty(wishlist.length+1);
+    
+  }
+
+  function deleteitem(item){
+    item.cartqty = 0;
+    let remainingItems = cartItems.filter((allitem)=>{
+        return (item.id !== allitem.id); 
+    })
+    setCartItems(remainingItems);
+    setCartqty(cartqty-1);
+  }
+
+  function removewishlist(item){
+    item.wishlist = false;
+    let remainingItems = wishlist.filter((curitem)=>{
+        return (item.id !== curitem.id); 
+    })
+    setWishlist(remainingItems);
+    setWishlistqty(wishlistqty-1);
+  }
+
   return (
     <div className="app">
-      {/* <Switch>
-          <Route exact path="/" component="Home" />
-          <Route path="/product" component="Product" />
-      </Switch> */}
-      <Routes>
-          <Route  path="/" element={<Home/>} />
-          <Route path="/product" element={<Product/>} />
-          <Route path="/register" element={<Register/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/cart" element={<Cart/>} />
-          <Route path="/productlist" element={<ProductList/>} />
 
-          
-      </Routes>
+        <Header cartqty={cartqty} wishlistqty={wishlistqty}/>
+
+      {/* <productContext.provider >
+      </productContext.provider> */}
+
+        <MyRouter cartqty={cartqty} cartItems={cartItems} addtocart={addtocart} deleteitem={deleteitem} wishlist={wishlist} addtowishlist={addtowishlist}  removewishlist={removewishlist}/>
+      
+      
+      <Footer/>
 
 
-        {/* <Home/> */}
-        {/* <Product/> */}
-        {/* <ProductList/> */}
-        {/* <Cart/> */}
-        
     </div>
   );
 }
